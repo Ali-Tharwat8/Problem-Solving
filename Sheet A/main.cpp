e2a8f02bac9d1440dee5716e0b4e61a3fde8b709
Ali-Tharwat8 <alitharwat128@gmail.com>
2025-06-14 18:54:29 +0300

    A. Boy or Girl

--- Sheet A/main.cpp
+++ Sheet A/main.cpp
@@ -3,32 +3,20 @@
 #include <cmath>
 #include <algorithm>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 int main()
 {
-	string one, two;
-	cin >> one >> two;
-	int i = 0;
-	for (; i < one.size(); i++)
-	{
-		if (tolower(one.at(i)) != tolower(two.at(i)))
-		{
-			if (tolower(one.at(i)) > tolower(two.at(i)))
-			{
-				cout << 1;
-				break;
-			}
-			else if (tolower(one.at(i)) < tolower(two.at(i)))
-			{
-				cout << -1;
-				break;
-			}
-		}
-	}
+	string name; cin >> name;
+	
+	set<char> distinct(name.begin(), name.end());
+	
+	if (distinct.size() % 2 == 0)
+		cout << "CHAT WITH HER!";
+	else
+		cout << "IGNORE HIM!";
 
-	if (i == one.size())
-		cout << 0;
 	return 0;
 }
