b590a23e0ffb2dfb3d0d214f2783eb3df0cf8b5a
Ali-Tharwat8 <alitharwat128@gmail.com>
2025-08-13 09:50:04 +0300

    Games

--- Sheet A/main.cpp
+++ Sheet A/main.cpp
@@ -9,16 +9,26 @@ using namespace std;
 
 int main()
 {
-	string name; cin >> name;
-	char prev = 'a';
+	int num; cin >> num;
+	vector<int> color1;
+	vector<int> color2;
+
+	for (int i = 0; i < num; i++) {
+		int color; cin >> color;
+		color1.push_back(color);
+		cin >> color;
+		color2.push_back(color);
+	}
+
 	int sum = 0;
-	for (char c : name) {
-		int diff = abs(c - prev);
-		if (diff <= 13)
-			sum += diff;
-		else
-			sum += (26 - diff);
-		prev = c;
+
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < num; j++) {
+			if (i == j)
+				continue;
+			if (color2[i] == color1[j])
+				sum++;
+		}
 	}
 	cout << sum << endl;
 	return 0;
