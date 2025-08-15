57bfb61a452d06edb5008c7367f4c3dbbdf2ae82
Ali-Tharwat8 <alitharwat128@gmail.com>
2025-08-15 10:28:46 +0300

    A. Die Roll

--- Sheet A/main.cpp
+++ Sheet A/main.cpp
@@ -4,19 +4,20 @@
 #include <algorithm>
 #include <vector>
 #include <set>
+#include <numeric>
 
 using namespace std;
 
 int main()
 {
-	string position, instructions; cin >> position >> instructions;
+	int num1, num2; cin >> num1 >> num2;
+	int num3 = 6 - max(num1, num2) + 1;
+	int den = 6;
 	
-	int steps = 0;
-	for (int i = 0; i < instructions.size(); ++i) {
-		if (position.at(steps) == instructions.at(i)) {
-			steps++;
-		}
-	}
-	cout << steps + 1 << endl;
+	int g = gcd(num3, den);
+	num3 /= g; den /= g;
+	cout << num3 << "/" << den << endl;
 	return 0;
 }
+
+
