1c2505646c0593b5eb8dbc8e3a58b24d2a4d6bf5
Ali-Tharwat8 <alitharwat128@gmail.com>
2025-08-17 20:26:23 +0300

    New Password

--- Sheet A/main.cpp
+++ Sheet A/main.cpp
@@ -10,29 +10,14 @@ using namespace std;
 
 int main()
 {
-	int num; cin >> num;
-	vector<int> member1;
-	vector<int> member2;
-	vector<int> member3;
+	string latters = "abcdefghijklmnopqrstuvwxyz";
+	int length, num; cin >> length >> num;
+	string password;
 
-	for (int i = 0; i < num; i++) {
-		int n; cin >> n;
-		if (n == 1)
-			member1.push_back(i + 1);
-		else if (n == 2)
-			member2.push_back(i + 1);
-		else
-			member3.push_back(i + 1);
+	for (int i = 0; i < length; i++) {
+		password += latters.at(i % num);
 	}
-
-	int teams = min(member1.size(), min(member2.size(), member3.size()));
-
-	cout << teams << endl;
-	for (int i = 0; i < teams; i++) {
-		cout << member1.at(i) << " "
-			<< member2.at(i) << " "
-			<< member3.at(i) << endl;
-	}
-
+	
+	cout << password << endl;
 	return 0;
 }
